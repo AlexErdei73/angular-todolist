b6aa46cae870366f8e552cbe10930058f0e9b747
Alex Erdei <mralexerdei@yahoo.co.uk>
2020-09-30 06:32:04 +0100

    add Todo class

---
+++ src/app/model/todo.ts
@@ -0,0 +1,52 @@
+export class Todo {
+    private _dueDate: Date;
+    private _notes: Array<string>;
+    private _priority: string;
+
+    constructor(private _title: string = '',
+        private _description: string = ''){
+        this._dueDate = new Date(0);
+        this._notes = [];
+    }
+
+    set title(t: string) {
+        this._title = t;
+    }
+    
+    get title() {
+        return this._title;
+    }
+
+    set description(text: string) {
+        this._description = text;
+    }
+    
+    get description() {
+        return this._description;
+    }
+
+    set dueDate(date: Date) {
+        this._dueDate = date;
+    }
+    
+    get dueDate() {
+        return this._dueDate;
+    }
+
+    set priority(text: string) {
+        this._priority = text;
+    }
+    
+    get priority() {
+        return this._priority;
+    }
+
+    set notes(arrayOfText: Array<string>) {
+        this._notes = arrayOfText;
+    }
+    
+    get notes() {
+        return this._notes;
+    }
+
+}
